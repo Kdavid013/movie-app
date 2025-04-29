5027d84169852b1e32565d10cff338692ec0f2f8
Kdavid013 <61146486+Kdavid013@users.noreply.github.com>
2025-04-29 16:01:52 +0200

    genre request in declerative mode

--- movie-app/ui/genre-section/GenreSectionViewModel.swift
+++ movie-app/ui/genre-section/GenreSectionViewModel.swift
@@ -6,6 +6,7 @@
 //
 import Foundation
 import InjectPropertyWrapper
+import Combine
 
 protocol ErrorViewModelProtocol{
     var alertModel: AlertModel? { get }
@@ -13,64 +14,143 @@ protocol ErrorViewModelProtocol{
 
 protocol GenreSectionViewModelProtocol: ObservableObject {
     var genres: [Genre] { get }
-    func fetchGenres() async
+    
 }
 
 class GenreSectionViewModel: GenreSectionViewModelProtocol, ErrorViewModelProtocol{
     @Published var genres: [Genre] = []
     @Published var alertModel: AlertModel? = nil
     
-//    private var movieService: MovieServiceProtocol = MovieService()
+    private var cancellables = Set<AnyCancellable>()
+    
+    //    private var movieService: MovieServiceProtocol = MovieService()
     @Inject var movieService: MovieServiceProtocol
     
-    func fetchGenres() async{
-       do {
-           let request = FetchGenreRequest()
-           let genres = Environment.name  == .tv ? try await movieService.fetchTVGenres(req: request) : try await movieService.fetchGenres(req: request)
-//         visszahozza a programot a main threadre, hogy ne blokkolja a UI-t
-           DispatchQueue.main.async {
-               self.genres = genres
-           }
-       }
-       catch let error as MovieError
-       {
-           DispatchQueue.main.async {
-               self.alertModel = self.toAlertModel(error)
-           }
-       } catch{
-           DispatchQueue.main.async {
-               self.alertModel = self.toAlertModel(error)
-           }
-       }
-    }
+    //    func fetchGenres() async{
+    //       do {
+    //           let request = FetchGenreRequest()
+    //           let genres = Environment.name  == .tv ? try await movieService.fetchTVGenres(req: request) : try await movieService.fetchGenres(req: request)
+    //       visszahozza a programot a main threadre, hogy ne blokkolja a UI-t
+    //           DispatchQueue.main.async {
+    //               self.genres = genres
+    //           }
+    //       }
+    //       catch let error as MovieError
+    //       {
+    //           DispatchQueue.main.async {
+    //               self.alertModel = self.toAlertModel(error)
+    //           }
+    //       } catch{
+    //           DispatchQueue.main.async {
+    //               self.alertModel = self.toAlertModel(error)
+    //           }
+    //       }
+    //    }
     
     private func toAlertModel(_ error: Error) -> AlertModel{
         guard let error = error as? MovieError else{
             return AlertModel(
-             title: "alert.unexpected.title",
-             message: "alert.unexpected.text",
-             dismissButtonTitle: "alert.dismiss.button"
+                title: "alert.unexpected.title",
+                message: "alert.unexpected.text",
+                dismissButtonTitle: "alert.dismiss.button"
             )
         }
         switch error {
         case .invalidApiKeyError(let message):
             return AlertModel(
-             title: "alert.api.title",
-             message: message,
-             dismissButtonTitle: "alert.dismiss.button"
+                title: "alert.api.title",
+                message: message,
+                dismissButtonTitle: "alert.dismiss.button"
             )
         case .clientError:
             return AlertModel(
-             title: "Client Error",
-             message: error.localizedDescription,
-             dismissButtonTitle: "alert.dismiss.button"
+                title: "Client Error",
+                message: error.localizedDescription,
+                dismissButtonTitle: "alert.dismiss.button"
             )
         default:
             return AlertModel(
-             title: "alert.unexpected.title",
-             message: "alert.unexpected.text",
-             dismissButtonTitle: "alert.dismiss.button"
+                title: "alert.unexpected.title",
+                message: "alert.unexpected.text",
+                dismissButtonTitle: "alert.dismiss.button"
             )
         }
     }
+    
+    init() {
+        let request = FetchGenreRequest()
+        
+        //        future publisher, ami genre kat ad ki egy tömbben
+        let future = Future<[Genre], Error> { future in
+            Task {
+                do {
+                    let genres = try await self.movieService.fetchGenres(req: request)
+                    future(.success(genres))
+                } catch {
+                    future(.failure(error))
+                }
+            }
+        }
+        
+        let futureTV = Future<[Genre], Error> { future in
+            Task {
+                do {
+                    let genres = try await self.movieService.fetchTVGenres(req: request)
+                    future(.success(genres))
+                } catch {
+                    future(.failure(error))
+                }
+            }
+        }
+        
+        future
+            .flatMap ({ genres in
+                futureTV.map{ genresTV in
+                    (genres,genresTV)
+                }
+            })
+            .receive(on: RunLoop.main)
+        //        completion blokk megvizsgáljuk a sink válasza milyen tipusu
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    self.alertModel = self.toAlertModel(error)
+                case .finished:
+                    break
+                }
+            } receiveValue: {[weak self] genres,genresTV in
+                self?.genres = genres + genresTV
+            }
+            .store(in: &cancellables)
+    }
+    
+    //    init() {
+    //            let request = FetchGenreRequest()
+    //
+    //            let publisher = PassthroughSubject<[Genre], Error>()
+    //
+    //            Task {
+    //                do {
+    //                    let genres = try await self.movieService.fetchTVGenres(req: request)
+    //                    publisher.send(genres)
+    //                    publisher.send(completion: .finished) // FONTOS: befejezés
+    //                } catch {
+    //                    publisher.send(completion: .failure(error)) // Hiba küldése
+    //                }
+    //            }
+    //
+    //            publisher
+    //                .receive(on: RunLoop.main)
+    //                .sink { completion in
+    //                    switch completion {
+    //                    case .failure(let error):
+    //                            self.alertModel = self.toAlertModel(error)
+    //                    case .finished:
+    //                            break
+    //                    }
+    //                } receiveValue: { genres in
+    //                    self.genres = genres
+    //                }
+    //                .store(in: &cancellables)
+    //        }
 }
